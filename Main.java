8e73171757285c7711689e20965892367058cf06
dlyons5 <dominic.lyons@patelhs.org>
2020-10-12 12:50:16 +0000

    I started adding a scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
-class Main{
-   public static void main(String[] args)
+class Main(){ public class main() {} }
+
+  import java.util.scanner;
+  
+    public static void main(String[] args)
    {
+     Scanner scan = new Scanner(System.in); 
+    
+    System.out.println(" choose a number ");
+    scan.next();
+    
      boolean isCorrectMatch = false;
      if (isCorrectMatch)
      {
